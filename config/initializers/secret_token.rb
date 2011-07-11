d1d4360aaca55d4cb4e4ce306b25a9d514b9f8b1
dxkxzx <dxkxzx@gmail.com>
2011-07-11 14:55:36 +0400

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oddschecker::Application.config.secret_token = '1ade3955b17de9098c38a351f211ab158232f8fca8ddc6d852c978a262a67b95fd8b82be67368479034524d3ff617d7dd3675c86193f6dec633be3e4e1161e8c'
