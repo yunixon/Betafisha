3389154c5062150320b54d67cc13930cffb23caa
dxkxzx <dxkxzx@gmail.com>
2011-07-21 17:55:39 +0400

    gemfile.lock with killed x86-mingw32

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
+
+config.gem "nokogiri"
+
 require File.expand_path('../application', __FILE__)
+
+
 
 # Initialize the rails application
 Oddschecker::Application.initialize!
