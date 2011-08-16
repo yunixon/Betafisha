f5f873fbe1d131e09f2ed8c64df5416c8053a582
dxkxzx <dxkxzx@gmail.com>
2011-08-16 18:43:55 +0400

    daemon switched off

--- app/controllers/bets_loader_controller.rb
+++ app/controllers/bets_loader_controller.rb
@@ -2,14 +2,12 @@
 
 class BetsLoaderController < ApplicationController
 
-   
+    layout 'admin'
     def api_test
       #respond_to do |format|
         #format.html {#@coefficients_info = BookmakerCoefficients.find(:all, :order => "id desc", :limit => 250) }
       #  format.js { @coefficients_info = BookmakerCoefficients.find(:all, :order => "id desc", :limit => 250) }
       #end
     end
-    
-
-  
+      
 end
