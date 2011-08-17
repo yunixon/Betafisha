56227c09f1075528ca75ca1d8f00e73d28ddb6cc
dxkxzx <dxkxzx@gmail.com>
2011-08-17 22:09:55 +0400

    db structure refactoring

--- app/models/odd.rb
+++ app/models/odd.rb
@@ -2,6 +2,9 @@ class Odd < ActiveRecord::Base
   
   attr_accessible :name, :priority
   
-  belongs_to :odd_type, :bookmaker, :team, :event
+  belongs_to :odd_type 
+  belongs_to :bookmaker 
+  belongs_to :team 
+  belongs_to :event
   
 end
