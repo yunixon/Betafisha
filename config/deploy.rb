cf2d879e47583f98199760518b520ca513cefd2e
dxkxzx <dxkxzx@gmail.com>
2011-08-27 19:19:29 +0400

    changed deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-set :application, "betafisha.com"
+set :application, "Betafisha"
 
 set :user, "root"
 set :use_sudo, false
@@ -11,19 +11,21 @@ set :repository,  "git@github.com:axgusev/Betafisha.git"
 set :branch, "master"
 set :ssh_options, { :forward_agent => true }
 
+default_run_options[:pty] = true 
+
 role :web, "188.127.231.233"
                           # Your HTTP server, Apache/etc
 role :app, "188.127.231.233"                          # This may be the same as your `Web` server
 role :db,  "188.127.231.233", :primary => true # This is where Rails migrations will run
 
 
-#after "deploy", "deploy:bundle_gems"
-#after "deploy:bundle_gems", "deploy:restart" 
+after "deploy", "deploy:bundle_gems"
+after "deploy:bundle_gems", "deploy:restart" 
 
 namespace :deploy do
-   #task :bundle_gems do 
-   #	run "cd #{deploy_to}/current && bundle install vendor/gems"
-   #end
+   task :bundle_gems do 
+   	run "cd #{deploy_to}/current && bundle install vendor/gems"
+   end
    task :start do ; end
    task :stop do ; end
    task :restart, :roles => :app, :except => { :no_release => true } do
