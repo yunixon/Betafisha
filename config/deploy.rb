6139d40e970ba02708d5d3835564cca0035fd859
dxkxzx <dxkxzx@gmail.com>
2011-08-27 18:45:17 +0400

    changed deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,30 +1,32 @@
-set :application, "betafisha"
+set :application, "betafisha.com"
 
-set	:deploy_to, "/var/www/Betafisha"
+set :user, "root"
+set :use_sudo, false
+set	:deploy_to, "/var/www/#{application}"
+
+set :deploy_via, :remote_cache
 
 set :scm, :git
 set :repository,  "git@github.com:axgusev/Betafisha.git"
 set :branch, "master"
-set :deploy_via, :remote_cache
-
-set :user, "axgusev"
 set :ssh_options, { :forward_agent => true }
-default_run_options[:pty] = true
 
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+role :web, "188.127.231.233"
+                          # Your HTTP server, Apache/etc
+role :app, "188.127.231.233"                          # This may be the same as your `Web` server
+role :db,  "188.127.231.233", :primary => true # This is where Rails migrations will run
 
-role :web, "betafisha.com"                          # Your HTTP server, Apache/etc
-role :app, "betafisha.com"                          # This may be the same as your `Web` server
-role :db,  "betafisha.com", :primary => true # This is where Rails migrations will run
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+#after "deploy", "deploy:bundle_gems"
+#after "deploy:bundle_gems", "deploy:restart" 
 
-# If you are using Passenger mod_rails uncomment this:
- namespace :deploy do
+namespace :deploy do
+   #task :bundle_gems do 
+   #	run "cd #{deploy_to}/current && bundle install vendor/gems"
+   #end
    task :start do ; end
    task :stop do ; end
    task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
- end
- end
+     run "touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
