ee8e533c02cd43f2312c6dabdef70adff5ebc1a3
graffzon <graffzon@gmail.com>
2011-08-28 12:46:42 +0400

    betredkings parser

---
+++ lib/daemons/betredkings.rb
@@ -0,0 +1,76 @@
+begin
+#!/usr/bin/env ruby
+
+ENV["RAILS_ENV"] ||= "development"
+BOOKMAKER = "Betredkings"
+
+#spors of that bookmaker. Can be added more
+SPORTS = ["AFL", "Baseball", "Cycling", "Fighting", "Football", "GaelicFootball", "Handball", "Hurling",
+  "MotorRacing", "RugbyUnion", "Tennis", "Am.Football", "Basketball", "Darts", "Floorball",
+  "Futsal", "Golf", "HorseRacing", "IceHockey", "RugbyLeague", "Snooker", "Volleyball"]
+
+require File.dirname(__FILE__) + "/../../config/application"
+require 'open-uri'
+
+Rails.application.require_environment!
+
+$running = true
+Signal.trap("TERM") do 
+  $running = false
+end
+
+_bookmaker = Bookmaker.create :name => BOOKMAKER
+
+while $running do
+    SPORTS.each do |style|
+    doc = Nokogiri::HTML(open("http://aws2.betredkings.com/feed/#{style}.xml"))
+
+    doc.xpath('//sport').each do |sport|
+      _sport = Sport.create :name => sport['name'], :priority => 1
+      sport.children.each do |country|
+        _country = Country.create :name => country['name'], :priority => 1
+        country.children.each do |tournament|
+          _ligue = Ligue.create :name => tournament['name'], :priority => 1,
+            :sport_id => _sport.id, :country_id => _country.id
+          tournament.children.each do |match|
+            if match.name == 'match'
+              _match_name = ''
+              _match_name << match.children.children[0]['name'] << ' - ' << match.children.children[1]['name']
+              _event = Event.new :name => _match_name, :priority => 1
+              _event.ligue_id = _ligue.id
+              _event.save
+              match.children.each do |element|
+                case element.name
+                  when 'participants' then
+                    element.children.each do |participant|
+                      Team.create(:name => participant['name'], :priority => 1)
+                    end
+                  when 'matchodds' then
+                    element.children.each do |type|
+                      _type = BetType.create :name => type['type'], :priority => 1
+                      type.children.each do |odd|
+                        _bet = Bet.create :priority => 1
+                        _bet.name = type['scope']
+                        _bet.bet_type_id = _type.id
+                        _bet.event_id = _event.id
+                        _bet.bookmaker_id = _bookmaker.id
+                        _bet.odd = odd.text
+                        _bet.save
+                      end
+                    end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  #One time per day
+  sleep 86400
+end 
+
+end
+
+
