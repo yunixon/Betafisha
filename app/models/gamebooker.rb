d5f181cfe3cfbcc177c9082e44f9cd5d0f1e6342
graffzon <graffzon@gmail.com>
2011-09-01 23:28:14 +0400

    scopes for gamebooker model

--- app/models/gamebooker.rb
+++ app/models/gamebooker.rb
@@ -1,4 +1,7 @@
 class Gamebooker < ActiveRecord::Base
-  COMMON_ATTRIBUTES = ['sport', 'league', 'event', 'country', 'participant']
   has_one :betredking
+  scope :leagues, where(:table_name => 'league')
+  scope :sports, where(:table_name => 'sport')
+  scope :countries, where(:table_name => 'country')
+  scope :events, where(:table_name => 'event')
 end
