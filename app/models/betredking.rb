dc1d1976189fdb57b2610c0b60dd1c44e4576c76
graffzon <graffzon@gmail.com>
2011-09-04 00:43:48 +0400

    validations for bookmaker models

--- app/models/betredking.rb
+++ app/models/betredking.rb
@@ -1,3 +1,8 @@
 class Betredking < ActiveRecord::Base
   belongs_to :gamebooker
+  validates :element_name, :bookmaker_element => true
+  scope :leagues, where(:table_name => 'league')
+  scope :sports, where(:table_name => 'sport')
+  scope :countries, where(:table_name => 'country')
+  scope :events, where(:table_name => 'event')
 end
--- app/models/gamebooker.rb
+++ app/models/gamebooker.rb
@@ -1,5 +1,6 @@
 class Gamebooker < ActiveRecord::Base
   has_one :betredking
+  validates :element_name, :bookmaker_element => true
   scope :leagues, where(:table_name => 'league')
   scope :sports, where(:table_name => 'sport')
   scope :countries, where(:table_name => 'country')
