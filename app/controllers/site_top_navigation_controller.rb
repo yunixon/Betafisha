b8a142b5424105df0ab019f70ee4730a20520b90
dxkxzx <dxkxzx@gmail.com>
2011-09-20 10:52:26 +0400

    added coeff. to main page

--- app/controllers/site_top_navigation_controller.rb
+++ app/controllers/site_top_navigation_controller.rb
@@ -3,12 +3,21 @@ class SiteTopNavigationController < ApplicationController
   include SportsHelper
 
   def coefficients
-     #@title = I18n.t(:top_menu_coefficients)
-     @news_blocks = NewsBlock.all(:limit => 5)
-     @counter = NewsBlock.all.count
- 	   @sports = Sport.all
-
- 	   ActiveRecord::Base.logger.info get_common_sports.inspect
+    #@title = I18n.t(:top_menu_coefficients)
+    @news_blocks = NewsBlock.all(:limit => 5)
+    @counter = NewsBlock.all.count
+ 	  @sports = Sport.all
+    #@bet_types = BetType.all
+   # @bookmakers = Bookmaker.all
+    respond_to do |format|
+      format.html {
+        @top_event = League.first.events.first
+      }
+      format.js {
+        @league = League.find_by_id( params[:sport_id].gsub("league_", "").to_i )
+        @bookmakers = @league.events
+      }
+    end
   end
 
   def bookmakers
--- public/javascripts/accordian.js
+++ public/javascripts/accordian.js
@@ -1,24 +1,24 @@
 $j(document).ready(function() {
-	
-	$j('.acc_container').hide(); 
+
+	$j('.acc_container').hide();
 	$j('.leagues').hide();
 	$j("a.delete_sport_item").hide();
 	$j("a.edit_sport_item").hide();
-	//$j('.acc_trigger:first').addClass('active').next().show(); 
-	
-	
+	//$j('.acc_trigger:first').addClass('active').next().show();
+
+
 	$j('.acc_trigger').mouseover( function(){
 		$j("a.delete_sport_item").hide();
 		$j(this).find('a.delete_sport_item').show();
 		$j(this).find('a.edit_sport_item').show();
 	});
-	
+
 	$j('.acc_trigger').mouseout( function() {
 		$j("a.delete_sport_item").hide();
 		$j("a.edit_sport_item").hide();
 	});
-	
-	$j("a.delete_sport_item, a.delete_league_item, a.delete_country_item").click( function() {			
+
+	$j("a.delete_sport_item, a.delete_league_item, a.delete_country_item").click( function() {
         if(confirm(this.getAttribute("data-confirm"), this.getAttribute("title") )) {
             $j.post(this.href, {_method:'delete'}, null, "script");
             return false;
@@ -26,66 +26,66 @@ $j(document).ready(function() {
             return false;
         }
 	});
-	
+
 	$j("a.edit_sport_item").click( function() {
-        $j.ajax({ 
+        $j.ajax({
 	   		type: 'POST',
 	   		url: "sport_edit",
-	   		data: { id : this.getAttribute('id') }, 
-	   		dataType:'script' 
+	   		data: { id : this.getAttribute('id') },
+	   		dataType:'script'
 	   	});
         return false;
 	});
-	
+
 	$j("a.edit_country_item").click( function() {
-        $j.ajax({ 
+        $j.ajax({
 	   		type: 'POST',
 	   		url: "country_edit",
-	   		data: { id : this.getAttribute('id') }, 
-	   		dataType:'script' 
+	   		data: { id : this.getAttribute('id') },
+	   		dataType:'script'
 	   	});
         return false;
 	});
-	
+
 	$j("a.edit_league_item").click( function() {
-        $j.ajax({ 
+	      alert('a');
+        $j.ajax({
 	   		type: 'POST',
 	   		url: "league_edit",
-	   		data: { id : this.getAttribute('id') }, 
-	   		dataType:'script' 
+	   		data: { id : this.getAttribute('id') },
+	   		dataType:'script'
 	   	});
         return false;
 	});
 
 	$j('.acc_trigger').click( function() {
-
-		if( $j(this).next().is(':hidden') ) { 
-			$j('.acc_trigger').removeClass('active').next().hide(); 
-			$j(this).toggleClass('active').next().show(); 
+		if( $j(this).next().is(':hidden') ) {
+			$j('.acc_trigger').removeClass('active').next().hide();
+			$j(this).toggleClass('active').next().show();
 		}
-	
-		return false; 
+
+		return false;
 	});
-	
+
 	$j('.countries li.country a').click( function() {
-		
-		if( $j(this).parent().next().is(':hidden') ) { 
-			$j('.countries li.country a').parent().removeClass('active').next().hide(); 
-			$j(this).parent().toggleClass('active').next().show(); 
-		}
-	
-		return false; 
-	});
-	
-	$j('.leagues li.league').click( function() {
-		$j.ajax({ 
-	   		type: 'GET',
-	   		data: { sport_id :  this.getAttribute('id')  }, 
-	   		url: 'api_test', 
-	   		dataType:'script' 
-	   	});
-		return false; 
-	});
-});
 
+		if( $j(this).parent().next().is(':hidden') ) {
+			$j('.countries li.country a').parent().removeClass('active').next().hide();
+			$j(this).parent().toggleClass('active').next().show();
+		}
+
+		return false;
+	});
+
+	$j('.leagues li.league').click( function() {
+		$j.ajax({
+	   		type: 'GET',
+	   		data: { sport_id :  this.getAttribute('id')  },
+	   		url: '/coefficients',
+	   		dataType:'script'
+	   	});
+		return false;
+	});
+
+});
 
