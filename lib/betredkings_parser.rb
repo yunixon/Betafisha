e04c885c8e93ac9ee5cf2dabaaad97e2afccf465
graffzon <graffzon@gmail.com>
2011-09-28 13:52:08 +0400

    added relation participant/bet into betredkings parser

--- lib/betredkings_parser.rb
+++ lib/betredkings_parser.rb
@@ -10,7 +10,8 @@ class BetredkingsParser
   def self.parse!
     _bookmaker = Bookmaker.find_or_create_by_name BOOKMAKER
     _bookmaker.touch
-
+    _participants_array = []
+    
     COMMON_SPORTS.each do |style|
       doc = Nokogiri::HTML(open("http://aws2.betredkings.com/feed/#{style}.xml"))
 
@@ -33,6 +34,7 @@ class BetredkingsParser
             _league.touch
 
             tournament.children.each do |match|
+              _participants_array.clear
               if match.name == 'match'
                 _match_name = ''
                 _match_name << match.children.children[0]['name'] << ' - ' << match.children.children[1]['name']
@@ -46,6 +48,7 @@ class BetredkingsParser
                   case element.name
                   when 'participants' then
                     element.children.each do |participant|
+                      _participants_array << participant
                       _team_name = calculate_name(Betredking, participant['name'], 'participant')
                       _team = Participant.new(:name => _team_name, :priority => 1)
                       _team.event_id = _event.id
@@ -55,16 +58,25 @@ class BetredkingsParser
                   when 'matchodds' then
                     element.children.each do |type|
                       _type_name = calculate_name(Betredking, type['type'], 'bet_type', false)
-                      _type = BetType.find_or_create_by_name _type_name
-                      type.children.each do |odd|
-                        _bet = Bet.new :priority => 1
-                        _bet.name = type['scope']
-                        _bet.bet_type_id = _type.id
-                        _bet.event_id = _event.id
-                        _bet.bookmaker_id = _bookmaker.id
-                        _bet.odd = odd.text
-                        _bet.save
-                        _bet.touch
+                      if _type_name.present?
+                        _type = BetType.find_or_create_by_name _type_name
+                        type.children.each_with_index do |odd, i|
+                          _bet = Bet.new :priority => 1
+                          _bet.name = case odd['outcome']
+                                      when '1' then 
+                                        _participants_array[0]['name']
+                                      when '2' then
+                                        _participants_array[1]['name']
+                                      when 'X' then
+                                        'Draw'
+                                      end
+                          _bet.bet_type_id = _type.id
+                          _bet.event_id = _event.id
+                          _bet.bookmaker_id = _bookmaker.id
+                          _bet.odd = odd.text
+                          _bet.save
+                          _bet.touch
+                        end
                       end
                     end
                   end
