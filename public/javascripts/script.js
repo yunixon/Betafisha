e705224214a0eb7d7946e0379eabb7406a14654d
dxkxzx <dxkxzx@gmail.com>
2011-09-29 02:08:33 +0400

    added drop-down for tools

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -46,7 +46,19 @@ $(document).ready(function() {
 		}, 5000 );
 		*/
 
+    $("ul.dropdown li").hover(function(){
 
+        $(this).addClass("hover");
+        $('ul:first',this).css('visibility', 'visible');
+
+    }, function(){
+
+        $(this).removeClass("hover");
+        $('ul:first',this).css('visibility', 'hidden');
+
+    });
+
+    $("ul.dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
 
 
 
