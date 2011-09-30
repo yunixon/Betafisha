e115380854accf6515118de9434df435123377c4
graffzon <graffzon@gmail.com>
2011-09-30 14:23:51 +0400

    unification in unification =)

--- lib/unified_elements.rb
+++ lib/unified_elements.rb
@@ -20,11 +20,30 @@ require 'open-uri'
 Rails.application.require_environment!
 
 
+def calculate_common_name(name, type)
+  result = nil
+  if temp = Common.find(:first, :conditions => {:element_name => name})
+    result = temp
+  elsif temp = Common.find(:first, :conditions => ["element_name like :e and table_name = :t", {:e => "%#{name}%", :t => type}])
+    if temp.element_name.length > name.length
+      temp.update_attribute(:element_name, name)
+      result = temp
+    end
+  else
+    Common.find(:all, :conditions => {:table_name => type}).each do |s|
+      if name.downcase.include? s.element_name.downcase
+        result = s
+      end
+    end
+    result = Common.create(:table_name => type, :element_name => name) unless result
+  end
+end
+
 _bookmaker = Bookmaker.find_or_create_by_name BOOKMAKER
 
 #Separately finding all betypes for each bookmaker
 COMMON_BETTYPES.each_with_index do |c,i|
-  common = Common.create(:element_name => c, :table_name => 'bet_type')
+  common = calculate_common_name(c, 'bet_type')
   GAMEBOOKERS[i].each do |g|
     Gamebooker.create(:element_name => g, :table_name => 'bet_type', :common_id => common.id)
   end
@@ -43,19 +62,19 @@ end
 SPORTS.each do |style|
   doc = Nokogiri::HTML(open("http://xml.gamebookers.com/sports/#{style}.xml_attr.xml"))
   doc.xpath('//sport').each do |sport|
-    Common.create(:table_name => 'sport', :element_name => sport['name'])
+    calculate_common_name(sport['name'], 'sport')
     sport.children.each do |group|
       _country_name = group['name'].include?('~') ? 'World' : group['name'].split(' - ').first
       _league_name = group['name'].include?('~') ? group['name'].gsub('~','') : group['name'].split(' - ').last
-      Common.create(:table_name => 'country', :element_name => _country_name)
-      Common.create(:table_name => 'league', :element_name => _league_name)
+      calculate_common_name(_country_name, 'country')
+      calculate_common_name(_league_name, 'league')
       group.children.each do |event|
-        Common.create(:table_name => 'event', :element_name => event['name'])
+        calculate_common_name(event['name'], 'event')
         event.children.each do |bettype|
           if GAMEBOOKERS.flatten.include?(bettype['name'])
             bettype.children.each do |bet|
               _team_name = bet['outcome_name'] == 'X' ? 'Draw' : bet['outcome_name']
-              Common.create(:table_name => 'participant', :element_name => _team_name)
+              calculate_common_name(_team_name, 'participant')
             end
           end
         end
@@ -63,3 +82,4 @@ SPORTS.each do |style|
     end
   end
 end
+
