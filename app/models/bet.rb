ccb7cb5af033e6f35ad275effcb3f7993291443f
graffzon <graffzon@gmail.com>
2011-10-12 23:30:00 +0400

    added before save validator for bets of stan james

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -6,7 +6,21 @@ class Bet < ActiveRecord::Base
   belongs_to :bookmaker
   belongs_to :event
   belongs_to :participant
-  
+
   #find old elements, interval can be changed
   scope :old, where("updated_at < ?", Time.now - 1.hour)
+
+  before_save :verify_the_uniqueness
+
+
+  private
+
+  def verify_the_uniqueness
+    if self.bookmaker = Bookmaker.find(:first, :conditions => ['name = ?', 'StanJames'])
+      result = Bet.find(:all, :conditions =>
+      ['name = ? and bookmaker_id = ? and odd = ? and event_id = ?', self.name, self.bookmaker_id, self.odd, self.event_id])
+
+      return false if result.present?
+    end
+  end
 end
