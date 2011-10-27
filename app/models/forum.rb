0a831ac444371fc346d1ca46d872e1c6601938dd
dxkxzx <dxkxzx@gmail.com>
2011-10-27 15:48:30 +0400

    added forum

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,4 +1,6 @@
 class Forum < ActiveRecord::Base
   attr_accessible :name, :description
-  has_many :posts, :dependent => :destroy 
+  has_many :topics, :dependent => :destroy 
+
+  default_scope :order => 'forums.created_at DESC'
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,10 @@
 class Topic < ActiveRecord::Base
+
   attr_accessible :name
   belongs_to :forum
+  belongs_to :user
   has_many :posts, :dependent => :destroy 
+
+  default_scope :order => 'topics.created_at DESC'
+
 end
