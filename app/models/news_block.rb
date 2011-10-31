69b0664f08f28133c42d98a33a81c1cbbfd773b2
dxkxzx <dxkxzx@gmail.com>
2011-10-31 16:59:14 +0400

    cleaned project

--- app/models/news_block.rb
+++
@@ -1,9 +0,0 @@
-class NewsBlock < ActiveRecord::Base
-  
-  has_many :comments, :dependent => :destroy
-   
-  attr_accessible :title, :content, :image, :remote_image_url
-  mount_uploader :image, ImageUploader
-  
-  default_scope :order => 'news_blocks.created_at DESC'
-end
