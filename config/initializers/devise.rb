1b4b391dc6aee787d626e8847bf8dec302e41605
dxkxzx <dxkxzx@gmail.com>
2011-11-18 23:41:57 +0400

    fixed admin forms

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -172,7 +172,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-
+  
+  config.omniauth :twitter, "mb8vSIq1g6NMVp3qc0ksQ", "8eilJw7Id9kb4M4L6N5jl1f5out9luWppgLHAX0"
+  
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
